5bc4fe26164eca565d7f110edbb5fa7ad9f514cd
Dedenich <sergbrst@tut.by>
2020-03-02 13:44:20 +0300

    add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  else
+  return Math.min(...array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!array || array.length == 0) return 0;
+  else
+  return Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  reducer = (a, c) => a + c;
+  if (!array || array.length == 0) return 0;
+  else
+  return (array.reduce(reducer))/array.length;
 }
